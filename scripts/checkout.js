d24b598fe2e28a71bb10d500dc160f9a614239fe
vitoriakotowski <vitoriakotowski@gmail.com>
2024-10-10 12:11:58 -0300

    delete products from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,9 @@ cart.forEach((cartItem) => {
 
   console.log(matchingProduct);
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
         <div class="delivery-date">Delivery date: Wednesday, June 15</div>
 
         <div class="cart-item-details-grid">
@@ -96,6 +98,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
