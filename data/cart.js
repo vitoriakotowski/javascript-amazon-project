d19f197a69dbd91323f582aac22aacf355470f34
vitoriakotowski <vitoriakotowski@gmail.com>
2024-10-10 11:02:52 -0300

    implement functions and more modules

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let repeatedItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      repeatedItem = cartItem;
+    }
+  });
+
+  if (repeatedItem) {
+    repeatedItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
