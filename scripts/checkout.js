c8653a9a8049ae61cbae5f19b861519445f2bc35
vitoriakotowski <vitoriakotowski@gmail.com>
2024-10-23 15:16:17 -0300

    restore main view

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,18 +2,13 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
-// import "../data/cart-class.js";
-// import "../data/backend-practice.js";
 
 async function loadPage() {
   try {
-    // throw "error1";
     await loadProductsFetch();
 
     const value = await new Promise((resolve, reject) => {
-      // throw "error2";
       loadCart(() => {
-        // reject("error3")
         resolve("value3");
       });
     });
